335faeea12d4bef1e5f493aa893124f0b9f5f3f6
Dmitry <dmitrievd321@gmail.com>
2024-09-15 00:24:40 +0300

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, modi quos? Facilis alias vero animi obcaecati doloribus sint mollitia veritatis?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, modi quos? Facilis alias vero animi obcaecati doloribus sint mollitia veritatis?</p>
+        <div>{{ data }}</div>
+    </div>
+</template>
+
+<script setup>
+    const {data} = await useFetch('/api/currency/GBP');
+</script>
+
+<style scoped>
+    h2 {
+        margin: 0 0 20px 0;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, modi quos? Facilis alias vero animi obcaecati doloribus sint mollitia veritatis?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, modi quos? Facilis alias vero animi obcaecati doloribus sint mollitia veritatis?</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2 {
+        margin: 0 0 20px 0;
+        font-size: 36px;
+    }
+    p {
+        margin: 20px 0;
+    }
+
+</style>
